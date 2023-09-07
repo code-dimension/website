e7876b515af5d33172022dd06ee24c4781d935c3
Henrique Custódia <martinshenrique8@gmail.com>
2023-09-07 10:45:38 -0300

    build: adiciona a prop site em astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image()],
+  site: 'https://codedimension.com.br'
 });
