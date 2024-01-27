7f2373eeaeb6fa2dcac3d3e97e92aafdd200302e
Henrique Custódia <martinshenrique8@gmail.com>
2024-01-27 11:10:07 -0300

    fix: corrige favicon

--- site-config.ts
+++ site-config.ts
@@ -5,17 +5,11 @@ const title = "Code Dimension";
 const description =
   "A Code Dimension é uma plataforma focada em ensinar Front-End e Angular, através de conteúdos didáticos e relevantes!";
 
-const ogImage = `${siteUrl}/images/og-code.png`;
-
-const themeColor = "#03020f";
-
 const socialImage = `${siteUrl}/images/social-media-image.png`;
 
 export const siteConfig = {
   url: siteUrl,
   title,
   description,
-  ogImage,
-  themeColor,
   socialImage,
 };
