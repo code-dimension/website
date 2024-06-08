efb85fd7a59b35b98f8667299c4c9ca281cae107
Henrique Custódia <martinshenrique8@gmail.com>
2024-06-08 13:46:26 -0300

    wip: adiciona página do curso de testes automatizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import { siteConfig } from './site-config';
+import { siteConfig } from "./site-config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [
+    // estilos são aplicados em GlobalStyles.astro
+    tailwind({ applyBaseStyles: false }),
+    image(),
+  ],
   site: siteConfig.url,
 });
