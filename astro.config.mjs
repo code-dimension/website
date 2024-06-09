f993c931835f1074b4bbe61583ac52292f8d57a4
Henrique Custódia <martinshenrique8@gmail.com>
2024-06-09 19:56:03 -0300

    chore: migra pra astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import { siteConfig } from "./site-config";
 
 // https://astro.build/config
@@ -8,7 +7,6 @@ export default defineConfig({
   integrations: [
     // estilos são aplicados em GlobalStyles.astro
     tailwind({ applyBaseStyles: false }),
-    image(),
   ],
   site: siteConfig.url,
 });
