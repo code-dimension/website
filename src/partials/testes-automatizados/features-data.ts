277f480331ddcea1085bd02a2c35f677c845f90c
Henrique Custódia <martinshenrique8@gmail.com>
2024-10-21 11:00:55 -0300

    feat: adiciona mais caracteriísticas do curso

--- src/partials/testes-automatizados/features-data.ts
+++ src/partials/testes-automatizados/features-data.ts
@@ -8,11 +8,32 @@ interface Feature {
 
 export const features: Feature[] = [
   {
+    imageUrl: "/images/courses/testes-automatizados/unit-tests.png",
+    imageAlt: "Um cubo verde",
+    title: "Testes unitários da forma certa",
+    description:
+      "Testes unitários é um dos tópicos que mais geram dúvidas entre pessoas desenvolvedoras. Você aprenderá como criar testes, o que testar e, principalmente, como testar de forma eficiente. Tudo isso usando ferramentas famosas do mercado, como Jest e ng-mocks.",
+  },
+  {
+    imageUrl: "/images/courses/testes-automatizados/integration-tests.png",
+    imageAlt: "Várias curbos verdes agrupados",
+    title: "Testes de integração",
+    description:
+      "Testar como diferentes partes de uma aplicação se integram evitará com que você entregue código com possíveis bugs, que, provavelmente, seriam descobertos pelos usuários. Nesse curso você aprenderá como criar esses tipos de testes, utilizando ferramentas e técnicas muito conhecidas pelo mercado.",
+  },
+  {
+    imageUrl: "/images/courses/testes-automatizados/e2e-test.png",
+    imageAlt: "Um robô utilizando um computador",
+    title: "Testes End-to-End (E2E)",
+    description:
+      "Testar uma aplicação como um usuário real evitará problemas como regressão visual e má integração entre partes do projeto, além de trazer uma confiança gigantesca ao entregar o produto. Você aprenderá nesse curso, como criar esse tipo de teste utilizando uma das ferramentas mais respeitas atualmente: Cypress.",
+  },
+  {
     imageUrl: "/images/courses/testes-automatizados/strong-robot.png",
     imageAlt: "Robo muito forte",
     title: "Criar testes resilientes",
     description:
-      "Crie testes resilientes e evite ter que refazer os testes da sua aplicação sempre que detalhes mínimos mudarem!",
+      "Crie testes resilientes de verdade e evite ter que refazê-los sempre que detalhes mínimos no código do projeto mudarem!",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/two-paths.png",
@@ -20,7 +41,6 @@ export const features: Feature[] = [
     title: "Evitar detalhes de implementação",
     description:
       "Testes que focam no que importa para o usuário, evitando detalhes de implementação que não são relevantes para ele.",
-    orientation: "right",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/productively.png",
@@ -34,15 +54,14 @@ export const features: Feature[] = [
     imageAlt: "Uma pessoa planejando algo",
     title: "Test-Driven Development (TDD)",
     description:
-      "Test Driven Development (TDD) é uma abordagem que permite que você escreva testes antes de implementar o código, trazendo benefícios significativos para a testabilidade e manutenibilidade do projeto ao longo do tempo.",
-    orientation: "right",
+      "Aprenda a utilizar Test Driven Development (TDD) para criar os testes antes de implementar o código, trazendo benefícios significativos para a testabilidade e manutenibilidade do projeto ao longo do tempo.",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/mocking.png",
     imageAlt: "Uma pessoa com uma máscara",
     title: "Implementar técnicas de Mocking",
     description:
-      "Mocking é uma das técnicas mais relevantes quando o assunto é testes automatizados. No curso você aprenderá abordagens de Mocking como Shallow Rendering e Deep testing, que são extremamente eficientes e necessárias em qualquer projeto.",
+      "Mocking é uma das técnicas mais relevantes quando o assunto é testes automatizados. No curso você aprenderá abordagens de Mocking como Shallow Rendering, Stubs, Spies e também as diferenças em relação ao Deep Testing, que são extremamente eficientes e necessárias em qualquer projeto.",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/pyramid.png",
@@ -50,7 +69,6 @@ export const features: Feature[] = [
     title: "Pirâmide de testes",
     description:
       "A Pirâmide de testes é uma abordagem que organiza testes em níveis, começando com testes unitários e indo até End-to-End. Isso ensina a criar testes que são rápidos, confiáveis e evitam duplicação de código.",
-    orientation: "right",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/completed-tasks.png",
@@ -65,14 +83,13 @@ export const features: Feature[] = [
     title: "Utilizar Conventional Commits",
     description:
       "Conventional Commits padroniza e automatiza o histórico de commits, facilitando leitura, manutenção, e integração com CI/CD, além de gerar changelogs e gerenciar versões automaticamente.",
-    orientation: "right",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/stars.png",
     imageAlt: "Estrelas",
     title: "Utilizar ferramentas famosas do mercado",
     description:
-      "Durante este curso, você aprenderá a utilizar ferramentas amplamente utilizadas no mercado, como ng-mocks e Cypress. Essas ferramentas ajudam a simplificar a escrita de testes e garantem que você esteja utilizando as melhores práticas do mercado.",
+      "Durante este curso, você aprenderá a utilizar ferramentas amplamente utilizadas no mercado, como Angular, Nx, TailwindCSS, Jest, ng-mocks e Cypress. Com essas ferramentas, você irá estruturar o código do projeto e os seus testes de forma muito moderna e eficiente.",
   },
   {
     imageUrl: "/images/courses/testes-automatizados/reference.png",
@@ -80,6 +97,5 @@ export const features: Feature[] = [
     title: "Tornar-se especialista em testes",
     description:
       "Todo o conteúdo do curso mergulha profundamente em vários aspectos da implementação de testes em projetos. Você aprenderá como criar testes de unidade, integração e End-to-End em detalhes, seguindo as melhores práticas.",
-    orientation: "right",
   },
 ];
