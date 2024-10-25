3375b4796c0d7f3336f983eb8b77181fa90b8046
Henrique Custódia <martinshenrique8@gmail.com>
2024-10-25 09:26:21 -0300

    feat: adiciona sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { siteConfig } from "./site-config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    // estilos são aplicados em GlobalStyles.astro
-    tailwind({ applyBaseStyles: false }),
-  ],
-  site: siteConfig.url,
+  // estilos são aplicados em GlobalStyles.astro
+  tailwind({
+    applyBaseStyles: false
+  }), sitemap()],
+  site: siteConfig.url
 });
