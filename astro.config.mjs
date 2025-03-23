835cc6da4b7285af083d15b14056ad6e08881dbd
Henrique Custódia <martinshenrique8@gmail.com>
2025-03-22 22:26:43 -0300

    wip: adiciona sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { siteConfig } from "./site-config";
-
 import sitemap from "@astrojs/sitemap";
+
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // estilos são aplicados em GlobalStyles.astro
-  tailwind({
-    applyBaseStyles: false
-  }), sitemap()],
-  site: siteConfig.url
+    // estilos são aplicados em GlobalStyles.astro
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    sentry(),
+  ],
+  site: siteConfig.url,
 });
