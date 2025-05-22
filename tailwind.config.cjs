df2d3ea88e47c10c1f62fee0c2455f8f24eebc3c
Henrique Custódia <martinshenrique8@gmail.com>
2025-05-22 08:48:09 -0300

    chore: remove breakpoints que não estavam funcionando

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,25 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    theme: {
-      screens: {
-        xs: "360px",
-        // => @media (min-width: 360px) { ... }
-
-        sm: "720px",
-        // => @media (min-width: 720px) { ... }
-
-        md: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        lg: "1440px",
-        // => @media (min-width: 1440px) { ... }
-
-        xl: "1920px",
-        // => @media (min-width: 1920px) { ... }
-      },
-    },
-
     extend: {
       fontFamily: {
         inter: ["Inter"],
