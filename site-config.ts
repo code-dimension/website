f31a2bc35e0067b48908111738547053bf2d91fa
Henrique Custódia <martinshenrique8@gmail.com>
2025-05-30 10:50:08 -0300

    fix: aplica a url da origem dependendo do ambiente

--- site-config.ts
+++ site-config.ts
@@ -1,14 +1,17 @@
-const siteUrl = "https://codedimension.com.br";
+const prodSiteUrl = "https://codedimension.com.br";
+const devSiteUrl = "https://codedimension-website-dev-env.netlify.app";
 
 const title = "Code Dimension";
 
 const description =
-  "A Code Dimension é uma plataforma focada em ensinar Front-End e Angular, através de conteúdos didáticos e relevantes!";
+"A Code Dimension é uma plataforma focada em ensinar Front-End e Angular, através de conteúdos didáticos e relevantes!";
 
-const socialImage = `${siteUrl}/images/social-media-image.png`;
+const socialImage = `${prodSiteUrl}/images/social-media-image.png`;
+
+const isDevEnv = process.env.SITE_ENV === "DEV";
 
 export const siteConfig = {
-  url: siteUrl,
+  url: isDevEnv ? devSiteUrl : prodSiteUrl,
   title,
   description,
   socialImage,
