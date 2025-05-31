3ac4c17964ad2f64af0a4f1f500e824ef8a6d438
Henrique Custódia <martinshenrique8@gmail.com>
2025-05-31 20:57:57 -0300

    fix: corrige badges

--- src/shared/data/courses.ts
+++ src/shared/data/courses.ts
@@ -1,7 +1,7 @@
 export interface Course {
   imageUrl: string;
   imageAlt: string;
-  badgeClass: string;
+  badgeClass: 'badge-info' | 'badge-warning' | 'badge-accent';
   badgeText: string;
   title: string;
   description: string;
@@ -12,8 +12,8 @@ export const courses: Course[] = [
   {
     imageUrl: "/images/courses/angular-moderno/angular-moderno-banner-card-home.webp",
     imageAlt: "Angular Moderno",
-    badgeClass: "badge-warning",
-    badgeText: "Pré-venda",
+    badgeClass: "badge-info",
+    badgeText: "Lista de espera",
     title: "Angular Moderno",
     description:
       "Aprenda todas as novidades do Angular 20 e se torne um desenvolvedor mais produtivo e eficiente no seu dia a dia!",
@@ -33,7 +33,7 @@ export const courses: Course[] = [
     imageUrl: "/images/courses/ng17.png",
     imageAlt: "Curso de Angular 17",
     badgeClass: "badge-accent",
-    badgeText: "gratuito",
+    badgeText: "Gratuito",
     title: "Curso de Angular 17",
     description:
       "Construa uma aplicação para gerenciar produtos e aprenda conceitos fundamentais amplamente usados pelo mercado.",
