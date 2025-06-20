557610b4fd4edfc6b9899e567b8f2e40c6c2c3e0
Henrique Custódia <martinshenrique8@gmail.com>
2025-06-20 13:58:40 -0300

    feat: adiciona seções de precos

---
+++ src/partials/angular-moderno/sections/prices/prices.ts
@@ -0,0 +1,5 @@
+export const precoAVista =
+"https://pay.hotmart.com/G100307188R?off=wl9lg3jl&offDiscount=PREVENDA50";
+
+export const precoParcelado12x =
+  "https://pay.hotmart.com/G100307188R?offDiscount=PREVENDA50&split=12";
