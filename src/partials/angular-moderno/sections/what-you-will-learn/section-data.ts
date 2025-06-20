cbd9129b465e1b9e8b0441c4580aea596a89052f
Henrique Custódia <martinshenrique8@gmail.com>
2025-06-19 21:12:29 -0300

    feat: adiciona seção sobre o que será ensinado no curso

---
+++ src/partials/angular-moderno/sections/what-you-will-learn/section-data.ts
@@ -0,0 +1,62 @@
+interface Feature {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export const data: Feature[] = [
+  {
+    icon: "bi-layers",
+    title: "Estruture projetos profissionais com Standalone API",
+    description:
+      "Aprenda como organizar o projeto com Standalone Components, roteamento mais moderno, e deixe de lado a utilização de NgModules.",
+  },
+  {
+    icon: "bi-graph-up-arrow",
+    title: "Utilize uma arquitetura escalável",
+    description:
+      "Implemente o padrão corporativo de separação de responsabilidades, pronto para crescer sem virar uma bagunça.",
+  },
+  {
+    icon: "bi-shield-check",
+    title: "Implemente autenticação com segurança real",
+    description:
+      "Crie fluxos de autenticação utilizando guards de rota, interceptors, tokens, da mesma forma que uma empresa real faz.",
+  },
+  {
+    icon: "bi-diagram-3",
+    title: "Use reatividade com Signals",
+    description:
+      "Aprenda a usar os Signals para criar componentes reativos de forma simples e eficiente.",
+  },
+  {
+    icon: "bi-emoji-sunglasses",
+    title: "Use Chart.js com Defer Block",
+    description:
+      "Implemente gráficos interativos otimizados, carregados de forma inteligente apenas quando necessário.",
+  },
+  {
+    icon: "bi-boxes",
+    title: "Reutilize lógicas com <br> Directive Composition API",
+    description:
+      "Desenvolva diretivas mais reutilizáveis e crie componentes menos complexos.",
+  },
+  {
+    icon: "bi-lightning-charge",
+    title: "Otimize a performance com Zoneless Change Detection",
+    description:
+      "Remova Zone.js, use OnPush Strategy com Signals e alcance performance máxima na aplicação.",
+  },
+  {
+    icon: "bi-trophy",
+    title: "Construa CRUDs inteligentes",
+    description:
+      "Reutilize formulários com Reactive Forms, utilize Reoute Resolvers, crie campos com máscaras e validações avançadas.",
+  },
+  {
+    icon: "bi-code-slash",
+    title: "Utilize RxJS e Signals",
+    description:
+      "Aprenda a utilizar RxJS e os recursos de interopabibilidade que permitem criar lógicas reative com Rxjs e Signals.",
+  },
+];
