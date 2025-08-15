75cf89b5134a1bfe8982b8cee8238d483290c4d8
Henrique Custódia <martinshenrique8@gmail.com>
2025-08-15 12:06:41 -0300

    feat: adiciona novo checkout ao curso testes automatizados com Angular

--- src/partials/testes-automatizados/shared/prices.ts
+++ src/partials/testes-automatizados/shared/prices.ts
@@ -1,5 +1,5 @@
 export const precoAVista =
-"https://pay.hotmart.com/V95801413V?off=cxca8nxz&bid=1729686428410&offDiscount=LANÇAMENTO";
+  "https://pay.hotmart.com/V95801413V?off=cxca8nxz&bid=1729686428410&offDiscount=LANÇAMENTO&checkoutMode=10";
 
 export const precoParcelado12x =
-  "https://pay.hotmart.com/V95801413V?bid=1729685763026&offDiscount=LANÇAMENTO&split=12";
+  "https://pay.hotmart.com/V95801413V?bid=1729685763026&offDiscount=LANÇAMENTO&split=12&checkoutMode=10";
