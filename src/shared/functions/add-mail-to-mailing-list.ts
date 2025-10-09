0c3e8e3b46d54894fabdff9f691edc1dbb603e4c
Henrique Custódia <martinshenrique8@gmail.com>
2025-10-09 09:24:30 -0300

    feat: cria função addEmailToMailingList para padronizar a adição de emails

--- src/shared/functions/add-mail-to-mailing-list.ts
+++ src/shared/functions/add-mail-to-mailing-list.ts
@@ -1,22 +1,26 @@
 import axios from "axios";
 
 export interface AddEmailToMailingListPayload {
-  mailingListID: string;
+  mailingListId: string;
   name: string;
   email: string;
   phonenumber: string;
 }
 
-export function addEmailToMailingList({
-  mailingListID,
-  name,
-  email,
-  phonenumber,
-}: AddEmailToMailingListPayload) {
-  return axios.post("https://email-server.codedimension.com.br/add-email", {
-    mailingListID,
-    email: email,
-    name: name,
-    phone: phonenumber,
-  });
-}
+export function addEmailToMailingList({ mailingListId, name, email, phonenumber }: AddEmailToMailingListPayload) {
+    return new Promise((resolve) => {
+      axios
+        .post("https://n8nserver.vps.webdock.cloud/webhook/6e3a9809-16ad-45a0-b10a-f23ee775fd8f", {
+          mailingListId,
+          email: email,
+          name: name,
+          phone: phonenumber,
+        })
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((error) => {
+          resolve(error);
+        });
+    });
+  }
