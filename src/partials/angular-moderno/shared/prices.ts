defe8f61a97b0243261a614486a9fde4abe97a97
Henrique Custódia <martinshenrique8@gmail.com>
2025-10-23 13:15:27 -0300

    refactor: remove unused file

--- src/partials/angular-moderno/shared/prices.ts
+++
@@ -1,5 +0,0 @@
-export const precoAVista =
-"https://pay.hotmart.com/V95801413V?off=cxca8nxz&bid=1729686428410&offDiscount=LANÇAMENTO";
-
-export const precoParcelado12x =
-  "https://pay.hotmart.com/V95801413V?bid=1729685763026&offDiscount=LANÇAMENTO&split=12";
