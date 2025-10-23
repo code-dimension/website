fb419a3d5b2d73672a6fe0c7ab6db62ed4f2a889
Henrique Custódia <martinshenrique8@gmail.com>
2025-10-23 09:29:21 -0300

    feat: utiliza novo checkout com order-bump

--- src/partials/angular-moderno/sections/prices/prices.ts
+++ src/partials/angular-moderno/sections/prices/prices.ts
@@ -1,5 +1,5 @@
 export const precoAVista =
-  "https://pay.hotmart.com/G100307188R?off=ctb8ix46&offDiscount=PREVENDA50";
+  "https://pay.hotmart.com/G100307188R?checkoutMode=10&off=ctb8ix46&offDiscount=PREVENDA50";
 
 export const precoParcelado12x =
-  "https://pay.hotmart.com/G100307188R?off=wl9lg3jl&offDiscount=PREVENDA50&split=12";
+  "https://pay.hotmart.com/G100307188R?checkoutMode=10&off=wl9lg3jl&offDiscount=PREVENDA50&split=12";
