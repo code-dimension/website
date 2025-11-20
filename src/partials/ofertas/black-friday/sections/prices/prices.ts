81cb38ee6e1806837568bc5598a4c655df38e0c6
Henrique Custódia <martinshenrique8@gmail.com>
2025-11-20 19:32:29 -0300

    feat: adiciona página da black friday

---
+++ src/partials/ofertas/black-friday/sections/prices/prices.ts
@@ -0,0 +1,4 @@
+export const precoAVista = "https://pay.hotmart.com/G100307188R?checkoutMode=10&off=ctb8ix46&offDiscount=PREVENDA50";
+
+export const precoParcelado12x =
+  "https://pay.hotmart.com/G100307188R?checkoutMode=10&off=wl9lg3jl&offDiscount=PREVENDA50&split=12";
