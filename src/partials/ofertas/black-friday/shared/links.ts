695c0d5e47228d6793e1fb284da05a77b3446850
Henrique Custódia <martinshenrique8@gmail.com>
2025-11-21 20:14:04 -0300

    feat: adiciona passos para verificar o usuario

---
+++ src/partials/ofertas/black-friday/shared/links.ts
@@ -0,0 +1,3 @@
+export const checkoutParcelado12x = "https://pay.hotmart.com/N102633678M?bid=1763746639407";
+export const checkoutParcelado12xComCupom =
+  "https://pay.hotmart.com/N102633678M?bid=1763746639407&offDiscount=ALUNOSCODEDIMENSION";
